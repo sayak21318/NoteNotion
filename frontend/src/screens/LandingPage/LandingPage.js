c5198e239cd972f176162858d7f6ed588fb217f2
sayak21318 <sayak21318@gmail.com>
2023-02-21 08:13:53 +0530

    commit

--- frontend/src/screens/LandingPage/LandingPage.js
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -1,17 +1,44 @@
-import React from 'react'
-import { Container, Row } from 'react-bootstrap'
-import './LandingPage.css'
+import React from "react";
+import { Container, Row, Button } from "react-bootstrap";
+import "./LandingPage.css";
 
 export const LandingPage = () => {
-    return (
-        <div className='main'>
+  return (
+    <div className="main">
+      <Container>
+        <Row>
+          <div className="intro-text">
+            <div>
+              <h1 className="title">Welcome to Note<br/>Notion</h1>
+              <p className="subtitle">One safe place for all your notes</p>
+                      </div>
+                      
 
-            <Container>
-                <Row>
-                    Landing
-                </Row>
-            </Container>
+                      <div className="buttonContainer">
+                          <div>
+                              <a href="/login">
+                                  <Button size="lg" className="landingbutton">
+                                      Login
+                                  </Button>
+                              </a>
+                          </div>
+                          <div>
+                              <a href="/register">
+                                  <Button
+                                      size="lg"
+                                      className="landingbutton"
+                                      variant="outline-primary"
+                                  >
+                                      Signup
+                                  </Button>
+                              </a>
+                          </div>
+                      </div>
 
-        </div>
-    )
-}
+           
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
