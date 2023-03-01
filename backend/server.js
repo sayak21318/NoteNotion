19867d427182ed456ed073d9e40408e2c052af92
sayak21318 <sayak21318@gmail.com>
2023-03-02 03:00:36 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const notes = require('./data/notes');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes')
 
 const app = express();
 
 dotenv.config();
-connectDB()
+connectDB();
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('API is running...');
@@ -16,11 +18,13 @@ app.get('/api/notes', (req, res) => {
     res.json(notes)
 })
 
-app.get('/api/notes/:id', (req, res) => {
-    const note = notes.find(n => n._id === req.params.id);
+// app.get('/api/notes/:id', (req, res) => {
+//     const note = notes.find(n => n._id === req.params.id);
 
-    res.send(note)
-})
+//     res.send(note)
+// })
+
+app.use('/api/users', userRoutes)
 
 const PORT = process.env.PORT || 5000
 
